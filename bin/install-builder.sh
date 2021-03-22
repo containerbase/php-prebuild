30c8a838d29f85b6b5cdd1647124e890db171ef5
Michael Kriese <michael.kriese@visualon.de>
2021-03-22 07:18:17 +0100

    feat: add php builder

---
+++ bin/install-builder.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+
+echo "APT::Install-Recommends \"false\";" | tee -a /etc/apt/apt.conf.d/buildpack.conf
+echo "APT::Get::Upgrade \"false\";" | tee -a /etc/apt/apt.conf.d/buildpack.conf
+echo "APT::Get::Install-Suggests \"false\";" | tee -a /etc/apt/apt.conf.d/buildpack.conf
+
+
+apt-get update
+apt-get install -y \
+  build-essential \
+  ca-certificates \
+  curl \
+  dumb-init \
+  git \
+  file \
+  libbz2-dev \
+  libc-client2007e-dev \
+  libcurl4-gnutls-dev \
+  libjpeg-dev \
+  libkrb5-dev \
+  libmcrypt-dev \
+  libonig-dev \
+  libpng-dev \
+  libreadline-dev \
+  libssl-dev \
+  libtidy-dev \
+  libxml2-dev \
+  libxslt-dev \
+  libzip-dev \
+  pkg-config \
+  zlib1g-dev \
+  ;
+
+git clone https://github.com/php-build/php-build.git;
+PREFIX=/usr/local ./php-build/install.sh
+
+mkdir -p /usr/local/php /cache
---
+++ renovate.Dockerfile
@@ -0,0 +1,16 @@
+#-------------------------
+# renovate rebuild trigger
+#-------------------------
+
+# makes lint happy
+FROM scratch
+
+
+# renovate: datasource=docker depName=php versioning=docker
+ENV PHP_VERSION=7.3.27
+
+# renovate: datasource=docker depName=php versioning=docker
+ENV PHP_VERSION=7.4.16
+
+# renovate: datasource=docker depName=php versioning=docker
+ENV PHP_VERSION=8.0.3
